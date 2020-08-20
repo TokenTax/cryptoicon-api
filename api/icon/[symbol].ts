4cc97e5d578dd77a294eb13db0c7241251803f95
Alex Miles <alex@tokentax.us>
2020-08-19 20:13:22 -0600

    send 404

--- api/icon/[symbol].ts
+++ api/icon/[symbol].ts
@@ -5,18 +5,24 @@ import fs from "fs";
 export default (req: NowRequest, res: NowResponse) => {
   const { symbol } = req.query;
 
-  var filePath = path.join(
+  const filePath = path.join(
     __dirname,
     `../../public/icons/128/color/${symbol}.png`
   );
 
-  res.writeHead(200, {
-    "Content-Type": "image/png",
-    "Content-disposition": "inline; filename=" + symbol,
-  });
-
   var readStream = fs.createReadStream(filePath);
 
-  // We replaced all the event handlers with a simple call to readStream.pipe()
-  readStream.pipe(res);
+  readStream.on("open", function () {
+    res.writeHead(200, {
+      "Content-Type": "image/png",
+      "Content-disposition": "inline; filename=" + symbol,
+    });
+
+    readStream.pipe(res);
+  });
+
+  readStream.on("error", (err) => {
+    res.status(404);
+    res.send("error");
+  });
 };
